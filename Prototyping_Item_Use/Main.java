cf99293d756b40bbff4aad1b4f6831af36bf03a9
AeranicusCascadia <35699755+AeranicusCascadia@users.noreply.github.com>
2021-10-14 20:55:58 -0700

    Update Main.java

--- Prototyping_Item_Use/Main.java
+++ Prototyping_Item_Use/Main.java
@@ -1,1 +1,23 @@
 
+public class Main
+{
+    // static properties
+    
+    // static methods
+    
+    
+    
+    public static void main(String[] args) {
+        
+        Player player = new Player();
+        HealingPotion healpot = new HealingPotion();
+        
+        while (Player.playerHP > 0) {
+        System.out.println( player.getPlayerHP() + " hit points");
+        healpot.modHP(-3);
+        }
+        
+        System.out.println("You die!");
+        
+    }
+}
